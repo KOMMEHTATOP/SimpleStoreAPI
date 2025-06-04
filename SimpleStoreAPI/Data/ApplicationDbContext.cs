429ef2dab741329e4cc412e638d0ca1f577f1af4
Admin <123456+KOMMEHTATOP@users.noreply.github.com>
2025-06-04 21:54:58 +0500

    Add project files.

---
+++ SimpleStoreAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SimpleStoreAPI.Models;
+
+namespace SimpleStoreAPI.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; } = null!;
+    }
+}
---
+++ SimpleStoreAPI/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleStoreAPI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public required string Category { get; set; }
+        public required string CreatedByUserId { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
